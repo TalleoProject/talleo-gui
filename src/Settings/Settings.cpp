1d737040ce008433242e35fb8a37a4fe99f7d4fe
Mika Lindqvist <postmaster@raasu.org>
2021-09-29 06:51:37 +0300

    Remove tlo.cryptonote.club from default pool list.

--- src/Settings/Settings.cpp
+++ src/Settings/Settings.cpp
@@ -84,7 +84,6 @@ Settings& Settings::instance() {
 Settings::Settings() : m_p2pBindPort(0), m_cmdLineParser(nullptr) {
   m_defaultPoolList << "pool.talleo.org:3333";
   m_defaultPoolList << "pool.raasu.org:3333";
-  m_defaultPoolList << "tlo.cryptonote.club:3333";
 
   Style* lightStyle = new LightStyle();
   Style* darkStyle = new DarkStyle();
